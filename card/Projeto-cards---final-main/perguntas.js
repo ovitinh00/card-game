c1c8289457ed17b1de2e4e75ea2944ece543de7f
ovitinh00 <161320693+ovitinh00@users.noreply.github.com>
2024-11-04 08:00:10 -0300

    Add files via upload

---
+++ card/Projeto-cards---final-main/perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Geografia',
+    'Qual é o maior país em extensão territorial?',
+    'A Rússia'
+)
+
+criaCartao(
+    'Ciências',
+    'Qual é o planeta mais próximo do sol?',
+    'Mercúrio'
+)
+
+criaCartao(
+    'História',
+    'Em que ano o Brasil se tornou independente de Portugal?',
+    'Em 1822'
+)
+
+criaCartao(
+    'Educação Física',
+    'No futsal quantas faltas é dado o tiro livre?',
+    'Com 6 faltas'
+)
+
+criaCartao(
+    'Literatura',
+    'Quem escreveu o livro "Dom Quixote"?',
+    'Miguel de Cervantes'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é o maior órgão do corpo humano?',
+    'A pele',
+)
+
+criaCartao(
+    'Física',
+    'Qual a unidade de medida da força no Sistema Internacional?',
+    'Newton (N)',
+)
+
+criaCartao(
+    'Música',
+    'Quem é o autor da famosa canção "Garota de Ipanema"?',
+    'Tom Jobim e Vínicius de Moraes',
+)
+
+criaCartao(
+    'Mitologia',
+    'Na mitologia grega, quem é o deus do trovão?',
+    'Zeus',
+)
